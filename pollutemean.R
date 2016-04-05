7a3bf14cf76cbce1ea6c42eb3df158de67d9458e
hetalb <hetalbhagat+github@gmail.com>
2016-04-05 07:25:26 -0400

    Cleanup

--- pollutemean.R
+++ pollutemean.R
@@ -1,17 +1,18 @@
 pollutemean <- function(dir, pollutant, id = 1:332) {
   # Read file names from dir
   fns = list.files(dir,full.names = TRUE, include.dirs = FALSE)
-  
-  # Get length of vector "id" to get number of files
-  len <- length(id)
-  
+
+  # Prepare empty data frame  
   total_df <- data.frame()
+
   # Loop to get file names
   for(f in fns[id]) {
     # Read files 
     df <- read.csv(f)
     total_df <- rbind(total_df, df)
   }
+  
+  # find mean for all files, exclude NAs 
   mean_poll <- mean(total_df[[pollutant]], na.rm = TRUE)
   print (mean_poll)
 }
